789b62e7ac948b294d0034f8130bcd82b5862f03
vanh26hihi <vietanhdoan987@gmail.com>
2025-11-14 15:29:29 +0700

    them env

--- Du_Lich_CamasRungj/commons/env.php
+++ Du_Lich_CamasRungj/commons/env.php
@@ -9,6 +9,6 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'tour_du_lich');  // Tên database
+define('DB_NAME', 'tour_du_lich');  // Tên database tour_du_lich
 
 define('PATH_ROOT', __DIR__ . '/../');
